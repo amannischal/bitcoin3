2bed269bd0773ea5fac34641d7faf37c2e2b42de
anischal <anischal@seas.upenn.edu>
2021-10-18 21:06:51 -0400

    final

--- ethereum_query.py
+++ ethereum_query.py
@@ -13,28 +13,44 @@ else:
     print( "Failed to connect to Ethereum node!" )
 
 def get_transaction(tx):
-    tx = {}   #YOUR CODE HERE
+    tx = w3.eth.get_transaction(tx)
     return tx
 
 # Return the gas price used by a particular transaction,
 #   tx is the transaction
 def get_gas_price(tx):
-    gas_price = 1 #YOUR CODE HERE
+    tx = w3.eth.get_transaction(tx)
+    gas_price = tx['gasPrice']
     return gas_price
 
 def get_gas(tx):
-    gas = 1 #YOUR CODE HERE
+    tx = w3.eth.get_transaction_receipt(tx)
+    gas = tx['gasUsed']  # YOUR CODE HERE
     return gas
 
 def get_transaction_cost(tx):
-    tx_cost = 1 #YOUR CODE HERE
+    tx_cost = get_gas(tx) * get_gas_price(tx)  # YOUR CODE HERE
     return tx_cost
 
 def get_block_cost(block_num):
-    block_cost = 1  #YOUR CODE HERE
+    block = w3.eth.get_block(block_num)
+    txs = block['transactions']
+    block_cost = 0
+    for tx in txs:
+        block_cost += get_transaction_cost(tx)
+        # YOUR CODE HERE
     return block_cost
 
 # Return the hash of the most expensive transaction
 def get_most_expensive_transaction(block_num):
-    max_tx = HexBytes('0xf7f4905225c0fde293e2fd3476e97a9c878649dd96eb02c86b86be5b92d826b6')  #YOUR CODE HERE
+    #max_tx = HexBytes('0xf7f4905225c0fde293e2fd3476e97a9c878649dd96eb02c86b86be5b92d826b6')  #YOUR CODE HERE
+    block = w3.eth.get_block(block_num)
+    txs = block['transactions']
+    max = 0
+    max_tx = 0
+    for tx in txs:
+        if get_transaction_cost(tx) > max:
+            max = get_transaction_cost(tx)
+            max_tx = tx
+    max_tx = HexBytes(max_tx)  # YOUR CODE HERE
     return max_tx
